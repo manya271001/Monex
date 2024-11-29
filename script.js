40c7e93b0928b479cd034a04519fe48c8ee74c83
Manya Sharma <manya271001@gmail.com>
2024-11-29 23:47:13 +0530

    delete and upade operation done

--- script.js
+++ script.js
@@ -62,11 +62,13 @@ function login(){
     });
 
 // CRUD OPERATIONS
+
+// TO READ DATA
 async function fet(){
        let d= await fetch("http://localhost:3000/Transaction")
     let data =  await d.json()
     let finalData=data.map((t)=>`
-         <tr width="200px">
+         <tr width="200px" style="margin-top:20px">
             <td style="display:flex; gap:10px"><img src="${t.img}" alt="" height="20px" width="20px" style=" border-radius: 50%;"> ${t.recv}</td>
             <td>${t.date}</td>
             <td style="color:${getStatusColor(t.status)}">${t.status}</td>
@@ -94,3 +96,58 @@ function getStatusColor(status) {
             return 'black'; 
     }
 }
+
+// TO DELETE DATA
+async function mydelete(id){
+     await fetch(`http://localhost:3000/Transaction/${id}`,{
+        method:"DELETE",
+        headers: {
+        "Content-Type": "application/json"
+    }
+
+    })
+     .then(res=>alert("delete sucessfull ....!!!"))
+}
+
+// 
+async function updateData(id){
+    let data = await fetch(`http://localhost:3000/Transaction/${id}`)
+    let newdata = await data.json()
+    let selectedData=`
+    
+      <input type="text"  value="${newdata.id}" readonly> <br>
+     <input type="text"  id="img1" value="${newdata.img}"> <br>  
+       <input type="text"  id="rev1" value="${newdata.recv}"> <br>
+     <input type="text"  id="date1" value="${newdata.date}"> <br>
+     <input type="text"  id="status1" value="${newdata.status}"> <br>
+     <input type="text"  id="price1" value="${newdata.price}"> <br>
+     <input type="text"  id="type1" value="${newdata.type}"> <br>
+     <input type="submit" onclick="finalUpDate('${newdata.id}')" style="background-color: black;
+            color: rgb(53, 156, 200); padding: 20px 70px; border-radius:12px" margin-left:"30px "> <br>
+     
+
+      `  
+      document.querySelector("#editable").innerHTML=selectedData
+     }
+
+     function finalUpDate(id){
+        let fdata={
+            img:document.querySelector('#img1').value,
+            recv:document.querySelector('#rev1').value,
+            date:document.querySelector('#date1').value,
+            status:document.querySelector('#status1').value,
+            price:document.querySelector('#price1').value,
+            type:document.querySelector('#type1').value,
+        }
+        fetch(`http://localhost:3000/Transaction/${id}`,{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(fdata)
+        })
+        .then(r=>alert("updatedd....!!!!"))
+     }
+
+
+
